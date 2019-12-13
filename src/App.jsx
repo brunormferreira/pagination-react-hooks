c6d20b2e52525d1b7f45872fc5d4d5c236412095
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-13 16:57:59 -0300

    add: previous finish pagination logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Posts from './components/Posts';
+import Pagination from './components/Pagination';
 
 function App() {
   const [posts, setPosts] = useState([]);
 
   const [loading, setLoading] = useState(false);
 
-  // const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // const [postsPerPage, setPostsPerPage] = useState(10);
+  // eslint-disable-next-line no-unused-vars
+  const [postsPerPage, setPostsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -23,12 +25,21 @@ function App() {
     fetchPosts();
   }, [])
 
-  console.log(posts)
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="container mt-5">
-      <h1 className="text-primary mb-3">React pagination with hooks --- (Posts)</h1>
-      <Posts posts={posts} loading={loading} />
+      <h1 className="text-primary mb-3">React pagination posts with hooks</h1>
+      <Posts 
+        posts={currentPosts} 
+        loading={loading} />
+      <Pagination 
+        postsPerPage={postsPerPage} 
+        totalPosts={posts.length} 
+        paginate={paginate} />
     </div>
   );
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 const Posts = ({ posts, loading }) => {
   
-  if (loading) {
-    return <h1>Loading</h1>
-  }
-
+  if (loading) return <h1>Loading</h1>
+  
   return (
     <ul className="list-group mb-3">
       {posts.map(post => ( 
